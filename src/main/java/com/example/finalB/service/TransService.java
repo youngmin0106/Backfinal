61b458c714b9fd17f3ae5ed58ce14a946e20f38e
yoonhee <newyoonhee@gmail.com>
2023-10-30 18:01:54 +0900

    게시판구현중

--- src/main/java/com/example/finalB/service/TransService.java
+++ src/main/java/com/example/finalB/service/TransService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import com.example.finalB.domain.Trans;
 import com.example.finalB.repository.TransRepository;
