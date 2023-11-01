817ae30ab3e9e34c2a2a65317efdb5135f55f83a
yoonhee <newyoonhee@gmail.com>
2023-11-01 17:59:39 +0900

    상세페이지 구현완료

--- src/main/java/com/example/finalB/controller/TransController.java
+++ src/main/java/com/example/finalB/controller/TransController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,5 +41,13 @@ public class TransController {
 		return new ResponseEntity<>(transList, HttpStatus.OK);
 		
 		
+	}
+	
+	@GetMapping("/transDetail/{id}")
+	public ResponseEntity<?> getTrans(@PathVariable Integer id) {
+		
+		Trans trans = transService.getTrans(id);
+		
+		return new ResponseEntity<>(trans, HttpStatus.OK);
 	}
 }
--- src/main/java/com/example/finalB/service/TransService.java
+++ src/main/java/com/example/finalB/service/TransService.java
@@ -1,6 +1,7 @@
 package com.example.finalB.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,13 +16,18 @@ public class TransService {
 	private TransRepository transRepository;
 	
 	public void insertTrans(Trans trans) {
+		
 		transRepository.save(trans);
 	}
 	
 	public List<Trans> getTransList() {
 		
 		return transRepository.findAllByOrderByIdDesc();
-		
+	}
+	
+	public Trans getTrans(Integer id) {
+		 
+		return transRepository.findById(id).get();
 	}
 
 
