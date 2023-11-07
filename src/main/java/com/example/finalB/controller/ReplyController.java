939f56df8690bc53e948a244cb3fb3b736b0055d
youngmin <ranj987@naver.com>
2023-11-07 17:57:09 +0900

    댓글 백 수정구현완료

--- src/main/java/com/example/finalB/controller/ReplyController.java
+++ src/main/java/com/example/finalB/controller/ReplyController.java
@@ -1,10 +1,14 @@
 package com.example.finalB.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.finalB.DTO.ResponseDTO;
 import com.example.finalB.domain.Member;
 import com.example.finalB.domain.Reply;
 import com.example.finalB.service.ReplyService;
@@ -22,29 +25,36 @@ public class ReplyController {
 	@Autowired
 	private ReplyService replyService;
 	
+	 @GetMapping("reply/{no}/list")
+	    public ResponseEntity<List<Reply>> getReplyByNo(@PathVariable int no) {
+	        List<Reply> reply = replyService.getReplyByNo(no);
+	        return new ResponseEntity<>(reply, HttpStatus.OK);
+	    }
+	
+	
 	@PostMapping("/reply/{no}")
-	public @ResponseBody ResponseDTO<?> insertReply(@PathVariable int no , Reply reply , HttpSession session){
+	public  ResponseEntity<?> insertReply(@PathVariable int no , @RequestBody Reply reply , HttpSession session){
 		Member member = (Member)session.getAttribute("principal");
 
-		replyService.insertReply(no, reply, member);
+		replyService.insertReply(reply,no,member);
 		
-		return new ResponseDTO<>(HttpStatus.OK.value(), "댓글 등록 완료");
+		return new ResponseEntity<String>("댓글 등록 완료" ,HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/reply/{replyId}")	
-	public @ResponseBody ResponseDTO<?> deleteReply(@PathVariable int replyId) {
+	public ResponseEntity<?> deleteReply(@PathVariable int replyId) {
 		
 		replyService.deleteReply(replyId);
 		
-		return new ResponseDTO<>(HttpStatus.OK.value(), "댓글 삭제 완료");
+		return new ResponseEntity<String>("댓글 삭제 완료" ,HttpStatus.OK);
 	}
 	
 	@PutMapping("/reply")
-	public @ResponseBody ResponseDTO<?> updateReply(@RequestBody Reply reply) {
+	public @ResponseBody ResponseEntity<?> updateReply(@RequestBody Reply reply) {
 		
 		replyService.updateReply(reply);
 		
-		return new ResponseDTO<>(HttpStatus.OK.value(), "댓글 수정 완료");
+		return new ResponseEntity<String>("댓글 수정 완료", HttpStatus.OK);
 	}
 	
 	}
--- src/main/java/com/example/finalB/service/ReplyService.java
+++ src/main/java/com/example/finalB/service/ReplyService.java
@@ -1,5 +1,7 @@
 package com.example.finalB.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +20,18 @@ public class ReplyService {
 	private ReplyRepository replyRepository;
 	@Autowired
 	private OnetoOneRepositroy onetoOneRepositroy;
-
+	
+	   public List<Reply> getReplyByNo(int no) {
+	        return replyRepository.findByOnetooneNo(no);
+	    }
+	
 	// 댓글작성
-	public void insertReply(int no, Reply reply, Member member) {
+	public void insertReply( Reply reply ,int no , Member member) {
 
 		OneToOne oneToone = onetoOneRepositroy.findById(no).get();
 
 		reply.setOnetoone(oneToone);
-		reply.setMember(member);
-
+	
 		replyRepository.save(reply);
 	}
 
@@ -37,10 +42,7 @@ public class ReplyService {
 
 	@Transactional
 	public void updateReply(Reply reply) {
-		// 어차피 아이디로 리플리 가져오면 거기에 포스트아이디 있으니까 굳이 set할 필요없음
-		// Post post = (Post)postRepository.findById(reply.getPost().getId()).get();
-		// reply.setPost(post);
-
+	
 		Reply oriReply = (Reply) replyRepository.findById(reply.getId()).get();
 		oriReply.setContent(reply.getContent());
 
